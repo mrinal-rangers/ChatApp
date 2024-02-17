a0bc5328089ea5b96f40ad19896f9548c9104087
Mrinal Deb <mrinal437a@gmail.com>
2024-02-17 18:35:25 +0530

    pata nhi kya

--- public/src/Pages/Register.jsx
+++ public/src/Pages/Register.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import {Link} from "react-router-dom"
 const FormContainer = styled.div``;
 
 const Register = () => {
@@ -6,6 +7,9 @@ const Register = () => {
   const handleSubmit = (event)=>{
     event.preventDefault();
     alert("form");
+  }
+  const handleChange = (event)=>{
+    console.log(event);
   }
 
   return (
@@ -17,7 +21,12 @@ const Register = () => {
             <h1>Snappy</h1>
           </div>
           <label htmlFor="username" >Username</label>
-          <input type="text" placeholder="username" name="username" onChange={e=>changeHandler(e)} />
+          <input type="text" placeholder="username" name="username" onChange={e=>handleChange(e)} />
+          <input type="email" placeholder="email" name="email" onChange={e=>handleChange(e)} />
+          <input type="password" placeholder="password" name="password" onChange={e=>handleChange(e)} />
+          <input type="password" placeholder="confirm password" name="confirmPassword" onChange={e=>handleChange(e)} />
+          <button type ="submit "> Create User </button>
+          <span>Alreay have an account ?  <Link to="/login" > Login </Link> </span>
         </form>
       </FormContainer>
     </>
