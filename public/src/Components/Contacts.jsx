f455e1bc09c9909bb08180dbffc152b8c62db73f
Mrinal Deb <mrinal437a@gmail.com>
2024-02-24 04:22:04 +0530

    currently broken code of contacts section adding your own contact is yet remaining:

--- public/src/Components/Contacts.jsx
+++ public/src/Components/Contacts.jsx
@@ -1,11 +1,61 @@
 import { useState } from "react"
 import Logo from '../Assets/logo.svg';
 import styled from 'styled-components';
+import { useEffect } from "react";
+import Contacts from './Contacts';
 
 const Contacts = ({contacts,currUser}) => {
+  const [currUserName,setCurrUserName] = useState(undefined);
+  const [currUserImage,setCurrUserImage] = useState(undefined);
+  const [currSelected,setCurrSelected] = useState(undefined);
+  
+  useEffect(()=>{
+    if(currUser){
+      setCurrUserName(currUser.username);
+      setCurrUserImage(currUser.avatarImage);
+    }
+  },[currUser])
+
+  const changeCurrChat = (index,contacts)=>{
+
+  }
+
   return (
-    <div >Contacts div</div>
+    <>
+      {
+        (currUserImage && currUserName) ? <Container>
+          <div className="brand">
+            <img src={Logo} alt="logo"/>
+            <h3>Snappy</h3>
+          </div>
+          <div className="contacts">
+            {
+              contacts.map((contact,index)=>{
+                return (<>
+                  <div className={`contact ${ index ===currSelected ? "selected":""}`} key={index}>
+                  <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} alt="avatar" />
+                  </div>
+                  <div className="username">
+                    <h3> {contact.username} </h3>
+                  </div>
+                  </>)
+              })
+
+            }
+
+          </div>
+
+        </Container> : <div>
+
+        </div>
+      }
+
+    </>
   )
 }
+
+const Container = styled.div`
+
+`
 
 export default Contacts
